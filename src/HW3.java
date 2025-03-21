40714d039d523def161cdf680e5869250ab8b0d1
ErmekWasdDevelop <ermekwasd@gmail.com>
2025-03-21 12:56:32 +0600

    else if

--- src/HW3.java
+++ src/HW3.java
@@ -1,9 +1,9 @@
 public class HW3 {
     public static void main(String[] args) {
 
-        double[] arrayD = {123.15, 65.15, -5.94, 65.1, 2.9, // ПУНКТ 1
-                -4.45, 14.87, 612.1, -56.24, 48.85,
-                35.72, -1.24, 4.73, 312.1, -31.86};
+        double[] arrayD = {123.15, 65.15, -5.94, 65.10, 2.90, // ПУНКТ 1
+                -4.45, 14.87, 612.10, -56.24, 48.85,
+                35.71, -1.24, 4.73, 312.10, -31.86};
         int kolvo=0;
         double sum = 0;
         for (int i = 2; i < arrayD.length; i++) {
@@ -12,7 +12,7 @@ public class HW3 {
             }
             kolvo++;
             System.out.print(arrayD[i] + " ");
-            sum += Double.valueOf(arrayD[i]);
+            sum += arrayD[i];
         }
         System.out.println(" =  " + (sum / kolvo)); // ПУНКТ 2
     }
